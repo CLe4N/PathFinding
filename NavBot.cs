81945bb901fb363e201a9ab1b6f3875c04eac32a
CLe4N <64629943+CLe4N@users.noreply.github.com>
2020-11-02 01:34:48 +0700

    Add files via upload

---
+++ NavBot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavBot : MonoBehaviour
+{
+    [SerializeField]
+    Transform goal;
+    void Start()
+    {
+        var BotAgent = GetComponent<NavMeshAgent>();
+        BotAgent.destination = goal.position;
+    }
+
+}
